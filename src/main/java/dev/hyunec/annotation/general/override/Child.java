b9025b1895dd90cb269d69be7c4bae8ee5dbbfdc
Hyune-c <choi8608@gmail.com>
2025-02-15 21:51:19 +0900

    [Annotation, Reflection] General Annotation: Override

---
+++ src/main/java/dev/hyunec/annotation/general/override/Child.java
@@ -0,0 +1,8 @@
+package dev.hyunec.annotation.general.override;
+
+public class Child extends Parent {
+    @Override
+    public void printMessage() {
+        System.out.println("I'm child class");
+    }
+}
