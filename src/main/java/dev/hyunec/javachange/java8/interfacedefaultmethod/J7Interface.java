0512ac7410610166fea6618302adc49aaf5cb175
Hyune-c <choi8608@gmail.com>
2025-03-02 00:28:17 +0900

    [Java Version 의 변화] Interface Default Method

---
+++ src/main/java/dev/hyunec/javachange/java8/interfacedefaultmethod/J7Interface.java
@@ -0,0 +1,5 @@
+package dev.hyunec.javachange.java8.interfacedefaultmethod;
+
+public interface J7Interface {
+    void printClassName();
+}
---
+++ src/main/java/dev/hyunec/javachange/java8/interfacedefaultmethod/J8Class.java
@@ -0,0 +1,11 @@
+package dev.hyunec.javachange.java8.interfacedefaultmethod;
+
+import java.time.ZonedDateTime;
+
+public class J8Class implements J8Interface {
+
+    @Override
+    public String getTime() {
+        return ZonedDateTime.now(java.time.ZoneId.of("Asia/Seoul")).toString();
+    }
+}
